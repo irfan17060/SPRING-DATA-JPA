6f731cf395f6e6027c4b001dc725f47f409fba0a
irfan17060 <irfankhank17060@gmail.com>
2024-05-13 01:47:40 +0530

    Update DbClean.java

--- src/main/java/org/irfan/sdjpaintro/config/DbClean.java
+++ src/main/java/org/irfan/sdjpaintro/config/DbClean.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
+
 
 @Configuration
 @Profile("clean")
